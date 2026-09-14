e926922757ef23948aac1414b7040a8f70002ffe
Lancelot <flegact60132@gmail.com>
2026-09-14 12:00:41 +0800

    2026/09/14

--- src/networkReuest/Myaxios.js
+++ src/networkReuest/Myaxios.js
@@ -206,7 +206,6 @@ const prepareLatestExcelDownload = async (excelId) => {
 
   return result;
 };
-
 const acquireOnlyOfficePreview = async (documentId, tabId) =>
   axiosInstance.post('office/acquireOnlyOfficePreview', { documentId, tabId });
 
